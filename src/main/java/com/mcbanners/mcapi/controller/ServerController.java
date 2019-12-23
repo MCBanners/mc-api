d1fe100759a26aa724c6de4196d5b7b39cc5bb6f
darbyjack <admin@glaremasters.me>
2019-12-23 04:23:07 -0600

    Handle query params

--- src/main/java/com/mcbanners/mcapi/controller/ServerController.java
+++ src/main/java/com/mcbanners/mcapi/controller/ServerController.java
@@ -7,8 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,9 +21,9 @@ public class ServerController {
         this.serverStatusService = serverStatusService;
     }
 
-    @GetMapping(value = "/{host}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ServerStatus> getStatus(@PathVariable String host) {
-        ServerStatus status = serverStatusService.getStatus(host, 25565);
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ServerStatus> getStatus(@RequestParam String host, @RequestParam int port) {
+        ServerStatus status = serverStatusService.getStatus(host, port);
         if (status == null) {
             return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
         }
