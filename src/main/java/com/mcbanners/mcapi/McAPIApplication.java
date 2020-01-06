486d39a1a2e980c8974cdd0ebe2ea03c25a569c7
simpleauthority <jacob@algorithmjunkie.com>
2020-01-05 19:28:12 -0800

    Make MC API a Eureka client

--- src/main/java/com/mcbanners/mcapi/McAPIApplication.java
+++ src/main/java/com/mcbanners/mcapi/McAPIApplication.java
@@ -3,13 +3,13 @@ package com.mcbanners.mcapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableCaching
 @SpringBootApplication
+@EnableDiscoveryClient
 public class McAPIApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(McAPIApplication.class, args);
     }
-
 }
