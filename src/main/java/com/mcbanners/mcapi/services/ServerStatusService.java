4b794fbe50c0aebfbfe23deba7b65b04592ba91e
darbyjack <admin@glaremasters.me>
2020-01-10 22:16:11 -0600

    Trim excess from MOTD

--- src/main/java/com/mcbanners/mcapi/services/ServerStatusService.java
+++ src/main/java/com/mcbanners/mcapi/services/ServerStatusService.java
@@ -53,7 +53,7 @@ public class ServerStatusService {
             status.setMotd(new Motd());
 
             status.getMotd().setRaw(info.getDescription().getText().trim());
-            status.getMotd().setStripped(MotdUtils.stripColors(info.getDescription().getText()));
+            status.getMotd().setStripped(MotdUtils.stripColors(info.getDescription().getText()).trim());
             status.getMotd().setFormatted("Coming Soon");
 
             final ByteArrayOutputStream icon = new ByteArrayOutputStream();
