f2b9602bfbc83bcad15a75821b99dab189314f66
darbyjack <admin@glaremasters.me>
2020-01-15 10:37:51 -0600

    Cleaned up and fixed requests

--- src/main/java/com/mcbanners/gameserverapi/game/service/SourceGameService.java
+++ src/main/java/com/mcbanners/gameserverapi/game/service/SourceGameService.java
@@ -1,8 +1,6 @@
 package com.mcbanners.gameserverapi.game.service;
 
 import com.ibasco.agql.protocols.valve.source.query.client.SourceQueryClient;
-import com.ibasco.agql.protocols.valve.source.query.enums.SourceChallengeType;
-import com.ibasco.agql.protocols.valve.source.query.pojos.SourcePlayer;
 import com.ibasco.agql.protocols.valve.source.query.pojos.SourceServer;
 import com.mcbanners.gameserverapi.game.status.GameStatus;
 import com.mcbanners.gameserverapi.game.status.source.SourceGameStatus;
@@ -12,12 +10,8 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.net.InetSocketAddress;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 @Service
 @CacheConfig(cacheNames = {"source-game-service"})
@@ -29,53 +23,26 @@ public class SourceGameService extends GameService {
     @Override
     @Cacheable
     public GameStatus getStatus(String hostname, int port) {
-        CompletableFuture<SourceGameStatus> futureStatus = new CompletableFuture<>();
-        SourceQueryClient sourceQueryClient = new SourceQueryClient();
         InetSocketAddress address = new InetSocketAddress(hostname, port);
 
-        CompletableFuture<Integer> challengeFuture = sourceQueryClient.getServerChallenge(SourceChallengeType.PLAYER, address);
-
-        challengeFuture.whenComplete((challenge, challengeError) -> {
-            SourceGameStatus sourceGameStatus = new SourceGameStatus();
-            if (challengeError != null) {
-                System.out.println(challengeError.getMessage());
-                return;
-            }
-
-            CompletableFuture<List<SourcePlayer>> playersFuture = sourceQueryClient.getPlayers(challenge, address);
-            playersFuture.whenComplete((players, playerError) -> {
-                if (playerError != null) {
-                    System.out.println(playerError.getMessage());
-                    return;
-                }
-                sourceGameStatus.setPlayers(players);
-            });
-
-            CompletableFuture<Map<String, String>> rulesFuture = sourceQueryClient.getServerRules(challenge, address);
-            rulesFuture.whenComplete((rules, rulesError) -> {
-                if (rulesError != null) {
-                    System.out.println(rulesError.getMessage());
-                    return;
-                }
-                sourceGameStatus.setRules(rules);
-            });
-
-            CompletableFuture<SourceServer> infoFuture = sourceQueryClient.getServerInfo(address);
-            infoFuture.whenComplete((info, infoError) -> {
-                if (infoError != null) {
-                    System.out.println(infoError.getMessage());
-                    return;
-                }
-                BeanUtils.copyProperties(info, sourceGameStatus);
-            });
-            futureStatus.complete(sourceGameStatus);
-        }).join();
-
-        try {
-            return futureStatus.get(5, TimeUnit.SECONDS);
-        } catch (InterruptedException | ExecutionException | TimeoutException e) {
-            e.printStackTrace();
+        try (SourceQueryClient sourceQueryClient = new SourceQueryClient()) {
+            SourceGameStatus status = new SourceGameStatus();
+            SourceServer server = sourceQueryClient.getServerInfo(address).get();
+            status.setPlayers(sourceQueryClient.getPlayersCached(address).get());
+            Map<String, String> rules = sourceQueryClient.getServerRulesCached(address).get();
+            status.setRules(rules);
+            // Set this cause for some reason it doesn't
+            server.setAddress(address);
+            // Copy stuff over
+            BeanUtils.copyProperties(server, status);
+            // Update this
+            status.setHost(address.getHostString());
+            status.setPort(address.getPort());
+            status.setIcon(rules.get("headerimage"));
+            return status;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
         }
-        return null;
     }
 }
