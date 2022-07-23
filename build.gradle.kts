6359b4691c80b30748b30746b9888b712f2d4c02
darbyjack <admin@glaremasters.me>
2022-07-23 18:43:07 -0500

    Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    id("org.springframework.boot") version "2.7.2"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("java")
+}
+
+group = "com.mcbanners"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+repositories {
+    mavenCentral()
+    maven("https://jitpack.io")
+    maven("https://repo.opencollab.dev/maven-releases/")
+}
+
+dependencies {
+    implementation(platform("org.springframework.cloud:spring-cloud-dependencies:2021.0.3"))
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-cache")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+    implementation("com.github.ben-manes.caffeine:caffeine:3.1.1")
+    implementation("com.github.Steveice10:MCProtocolLib:1.19-1")
+    implementation("net.kyori:adventure-text-serializer-legacy:4.11.0")
+    implementation("com.google.guava:guava:31.1-jre")
+
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+}
+
+
+tasks {
+    bootJar {
+        archiveFileName.set("mcapi.jar")
+    }
+}
